b5993044903429d4ee607088cd2e7926a06143f3
Michael Crowley <nico404.site@gmail.com>
2023-08-28 15:32:41 -0400

    Installed Express, Mongodb, and Cors packages

---
+++ api/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/fullstacktodo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Connected to DataBase")).catch(console.error);
+
+const Todo = require("./models/todo");
+
+app.get("/todos", async (req, res) => {
+    const todos = await Todo.find();
+
+    res.json(todos);
+});
+
+app.post('/todo/new', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save();
+
+    res.json(todo);
+})
+
+app.delete('/todo/delete/:id', async (req, res) =>{
+    const result = await Todo.findByIdAndDelete(req.params.id);
+
+    res.json(result);
+})
+
+app.listen(3002, () => console.log("Server started on port 3002"));
